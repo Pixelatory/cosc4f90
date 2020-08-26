99e1482ada1215d2c5353fa0c3f54dc16aad4b47
BuildTools <unconfigured@null.spigotmc.org>
2020-08-25 21:24:32 -0400

    updated mgpso on desktop. fixed where archive would have the same values. now it'll check to make sure the value being added to the archive isn't already there. otherwise it can be saturated with a very dominant solution, and thus will be crowded. we don't want this

--- msamgpso.py
+++ msamgpso.py
@@ -1,10 +1,10 @@
 import math
 import random
 import copy
-from numba import cuda
-import numpy as np
+import concurrent.futures
 from typing import List, Tuple
-from util import getLongestSeqDict, genBitMatrix, numOfAlignedChars, numOfInsertedIndels, bitsToStrings, test1
+from util import getLongestSeqDict, genBitMatrix, numOfAlignedChars, numOfInsertedIndels, bitsToStrings, test1, test2, \
+    test3, test4, test5, test6, test7
 
 """
     MGPSO for the MSA Problem
@@ -111,6 +111,27 @@ def MSAMGPSO(seq, genInterval, n, w, c1, c2, c3, l, k, vmax, vmaxiterlimit, term
                     better = True
         return better
 
+    def theSame(x, y):
+        """Checks if two sets of position and bitstring values are the same.
+
+        Assumes that the position vector and bitstrings are of the same length.
+
+        :type x: Tuple[List[float], List[List[int]]]
+        :type y: Tuple[List[float], List[List[int]]]
+        :rtype: bool
+        """
+
+        for i in range(len(x[0])):
+            if x[0][i] != y[0][i]:
+                return False
+
+        for i in range(len(x[1])):
+            for j in range(len(x[1][i])):
+                if x[1][i][j] != y[1][i][j]:
+                    return False
+
+        return True
+
     def addToArchive(x):
         """Adds solution x to archive a if x is not dominated by any archive solutions.
 
@@ -124,12 +145,16 @@ def MSAMGPSO(seq, genInterval, n, w, c1, c2, c3, l, k, vmax, vmaxiterlimit, term
         nonlocal sArchive
         aDominated = []  # all the archive components that are dominated by x
 
-        # First, check that this new solution dominates every archive solution
+        # First, check that this new solution dominates every archive solution,
+        # and that the values (bitstring and position) aren't repeated
         for s in sArchive:
             if dominates(s[1], x[1]):
                 return
             elif dominates(x[1], s[1]):
                 aDominated.append(s)
+
+            if theSame((s[0], s[1]), x):
+                return
 
         # When the function reaches here, it's safe to say the solution dominates.
         # So, let's add it to archive.
@@ -233,10 +258,49 @@ def MSAMGPSO(seq, genInterval, n, w, c1, c2, c3, l, k, vmax, vmaxiterlimit, term
                     gBest[i]["pos"] = copy.deepcopy(position)
                     gBest[i]["bitstring"] = copy.deepcopy(bitstring)
 
+    def multiThreaded(i, j):
+        nonlocal pVelocities, pBitStrings, pPersonalBests, gBest, vmaxiterlimit, vmax, seq, colLength, genInterval, f
+        # Within each subswarm, update each particle's velocity, position, and personal best
+        # r1 and r2 are ~ U (0,1)
+        r1 = random.random()
+        r2 = random.random()
+        r3 = random.random()
+        a = archiveGuide()
+
+        # update velocity and positions in every dimension
+        for k in range(4):
+            pVelocities[i][j][k] = w * pVelocities[i][j][k] + \
+                                   r1 * c1 * (pPersonalBests[i][j][k] - pPositions[i][j][k]) + \
+                                   l * r2 * c2 * (gBest[i]["pos"][k] - pPositions[i][j][k]) + \
+                                   (1 - l) * r3 * c3 * (a[k] - pPositions[i][j][k])
+
+            # velocity clamping
+            if vmaxiterlimit < it:
+                if pVelocities[i][j][k] > vmax:
+                    pVelocities[i][j][k] = vmax
+                elif pVelocities[i][j][k] < -vmax:
+                    pVelocities[i][j][k] = -vmax
+
+            pPositions[i][j][k] = pPositions[i][j][k] + pVelocities[i][j][k]
+
+        bitstring = genBitMatrix(pPositions[i][j], seq, colLength, genInterval)
+
+        # update personal best if applicable
+        if i == 0:  # numOfAlignedChars
+            strings = bitsToStrings(bitstring, seq)
+            pBestStrings = bitsToStrings(pBitStrings[i][j], seq)
+            if f[i](strings) > f[i](pBestStrings):
+                pPersonalBests[i][j] = copy.deepcopy(pPositions[i][j])
+                pBitStrings[i][j] = copy.deepcopy(bitstring)
+        else:  # numOfInsertedIndels
+            if f[i](bitstring, seq) < f[i](pBitStrings[i][j], seq):
+                pPersonalBests[i][j] = copy.deepcopy(pPositions[i][j])
+                pBitStrings[i][j] = copy.deepcopy(bitstring)
+
     # This is where the iterations begin
     it = 0  # iteration count
     while it < maxIter:
-        print(it)
+        # print(str(it) + " " + str(it / maxIter) + "%")
 
         # if the termination criteria is met, then stop the PSO and return values
         #  numOfAlignedChars                                            numOfInsertedIndels
@@ -247,44 +311,11 @@ def MSAMGPSO(seq, genInterval, n, w, c1, c2, c3, l, k, vmax, vmaxiterlimit, term
             for j in range(n):
                 addToArchive((pPositions[i][j], pBitStrings[i][j]))
 
-        # Within each subswarm, update each particle's velocity, position, and personal best
-        for i in range(len(f)):
-            for j in range(n):
-                # r1 and r2 are ~ U (0,1)
-                r1 = random.random()
-                r2 = random.random()
-                r3 = random.random()
-                a = archiveGuide()
-
-                # update velocity and positions in every dimension
-                for k in range(4):
-                    pVelocities[i][j][k] = w * pVelocities[i][j][k] + \
-                                           r1 * c1 * (pPersonalBests[i][j][k] - pPositions[i][j][k]) + \
-                                           l * r2 * c2 * (gBest[i]["pos"][k] - pPositions[i][j][k]) + \
-                                           (1 - l) * r3 * c3 * (a[k] - pPositions[i][j][k])
-
-                    # velocity clamping
-                    if vmaxiterlimit < it:
-                        if pVelocities[i][j][k] > vmax:
-                            pVelocities[i][j][k] = vmax
-                        elif pVelocities[i][j][k] < -vmax:
-                            pVelocities[i][j][k] = -vmax
-
-                    pPositions[i][j][k] = pPositions[i][j][k] + pVelocities[i][j][k]
-
-                bitstring = genBitMatrix(pPositions[i][j], seq, colLength, genInterval)
-
-                # update personal best if applicable
-                if i == 0:  # numOfAlignedChars
-                    strings = bitsToStrings(bitstring, seq)
-                    pBestStrings = bitsToStrings(pBitStrings[i][j], seq)
-                    if f[i](strings) > f[i](pBestStrings):
-                        pPersonalBests[i][j] = copy.deepcopy(pPositions[i][j])
-                        pBitStrings[i][j] = copy.deepcopy(bitstring)
-                else:  # numOfInsertedIndels
-                    if f[i](bitstring, seq) < f[i](pBitStrings[i][j], seq):
-                        pPersonalBests[i][j] = copy.deepcopy(pPositions[i][j])
-                        pBitStrings[i][j] = copy.deepcopy(bitstring)
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            for i in range(len(f)):
+                for j in range(n):
+                    executor.submit(multiThreaded, i, j)
+            # mapped = {executor.submit(multiThreaded, )}
 
         # update the global best after all positions were changed (synchronous PSO)
         for i in range(len(f)):
@@ -306,9 +337,21 @@ def MSAMGPSO(seq, genInterval, n, w, c1, c2, c3, l, k, vmax, vmaxiterlimit, term
     return sArchive
 
 
-wow = MSAMGPSO(test1, [-2.0, 2.0], 30, 0.729844, 1.49618, 1.49618, 1.49618, .5, 3, float('inf'), 500, [float('inf'), -float('inf')], 5000)
+def testing(seqs, i):
+    print("\nTest " + str(i))
+    t = MSAMGPSO(seqs, [-2.0, 2.0], 30, 0.729844, 1.49618, 1.49618, 1.49618, .5, 3, float('inf'), 500,
+                 [float('inf'), -float('inf')], 5000)
+    for res in t:
+        for string in bitsToStrings(res[1], seqs):
+            print(string)
+        print(numOfAlignedChars(bitsToStrings(res[1], seqs)))
+        print(numOfInsertedIndels(res[1], seqs))
 
-for res in wow:
-    print(numOfAlignedChars(bitsToStrings(res[1], test1)))
-    print(numOfInsertedIndels(res[1], test1))
 
+# testing(test1, 1)
+# testing(test2, 2)
+# testing(test3, 3)
+# testing(test4, 4)
+testing(test5, 5)
+testing(test6, 6)
+testing(test7, 7)
